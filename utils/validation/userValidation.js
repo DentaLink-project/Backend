18eb883ea3981bb30e8253ae63c2becbd3fd6202
saif-elhady <saifelhady2004@gmail.com>
2025-02-04 12:55:29 +0200

    feat: Add validation for userSchema

---
+++ utils/validation/userValidation.js
@@ -0,0 +1,22 @@
+import { check,validationResult } from "express-validator";
+
+export const signUpvalidator = [check('name').notEmpty().withMessage('Name is required'),
+check('email').notEmpty().withMessage('Email is required')
+    .isEmail().withMessage('Invalid email address'),
+check('password').notEmpty().withMessage('Password is required')
+    .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'), check('phone').notEmpty().withMessage('Phone number is required'),
+check('phone').notEmpty().withMessage('Phone number is required')
+    .isLength({ min: 11, max: 11 }).withMessage('Phone number must be exactly 11 digits'),
+    
+check('universityID').notEmpty().withMessage('National ID is required'),
+check('universityID').notEmpty().withMessage('universityID is required'),
+
+
+(req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+        return;
+    }
+    next();
+}];
