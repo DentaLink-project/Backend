5dd688cb534ed63eb992029d129c9d943a8f9c4a
saif-elhady <saifelhady2004@gmail.com>
2025-02-05 12:41:21 +0200

    feat: Add body parser implementation and use user routes for requets

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import database from './db.js';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 import cors from 'cors';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -14,6 +16,10 @@ app.use(cors({
     preflightContinue: false,
     optionsSuccessStatus: 204
 }));
+
+app.use(bodyParser.json());
+
+app.use('/api', userRoutes);
 
 async function startServer() {
     try {
