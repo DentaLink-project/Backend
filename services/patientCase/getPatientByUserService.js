b913b80a3979f4e9da93d9874f7050d151c5f05f
sohir <p@gmail.com>
2025-02-14 19:39:16 +0200

    feat: add patient search by title

---
+++ services/patientCase/getPatientByUserService.js
@@ -0,0 +1,18 @@
+import Patient from "../../models/patientCaseSchema.js";
+
+export const getPatientsByUser = async (user) => {
+    try {
+        const patients = await Patient.find({ createdBy: user._id })
+            .populate("createdBy", "name email");
+        const patientsWithFavStatus = patients.map(patient => ({
+            ...patient._doc,
+            isFavPatient: user.favorites.some(fav => fav.toString() === patient._id.toString()), // تحقق من كونه في المفضلة
+        }));
+
+        return patientsWithFavStatus;
+    } catch (error) {
+        throw new Error(`Error fetching patients: ${error.message}`);
+    }
+};
+
+
