ab9cf2ef30a2ebde7b04a3a6027482be06c559fd
sohir <p@gmail.com>
2025-02-14 21:59:44 +0200

    feat: add location field to Patient schema

--- models/patientCaseSchema.js
+++ models/patientCaseSchema.js
@@ -7,6 +7,7 @@ Schema({
     phone: { type: String, required: true },
     title: { type: String, required: true },  
     category: { type: String, required: true }, 
+    location: { type: String, required: true},
     description: { type: String },              
     file: { type: String },         
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "Student", required: true },
