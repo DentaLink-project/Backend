7e84119c7907d73b87b5802236a5c36650455602
sohir <p@gmail.com>
2025-02-14 20:00:09 +0200

    feat: Add fetchPatientsByTitle function to search patients by title

---
+++ controllers/patientController.js
@@ -0,0 +1,44 @@
+import Student from "../models/studentSchema.js";
+import { addPatient } from "../services/patientCase/addPatientServices.js";
+import { deletePatientById } from "../services/patientCase/deletePatientByIdServices.js";
+import { getFavoritePatients } from "../services/patientCase/getAllFavouritePatient.js";
+import { getAllPatients } from "../services/patientCase/getAllPatientServices.js";
+import { getPatientById} from "../services/patientCase/getPatientByIdServices.js";
+import { getPatientByTitle } from "../services/patientCase/getPatientByTitle.js";
+import { getPatientsByUser } from "../services/patientCase/getPatientByUserService.js";
+import toggleFavorite from "../services/patientCase/toggleFavouritePatient.js";
+import { updatePatientService } from "../services/patientCase/updatePatientDetails.js";
+
+//=============================**createPatient**===================================
+export const createPatient = async (req, res) => {
+    try {
+        const { name, title, age, gender, phone, category, description } = req.body;
+        const file = req.file;
+
+        const patient = await addPatient({
+            name,
+            title,
+            age,
+            gender,
+            phone,
+            category,
+            description,
+            file,
+            createdBy: req.student.id 
+        });
+
+        res.status(201).json({ message: "Patient added successfully", patient });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+//=============================**fetchPatientsByTitle**===================================
+export const fetchPatientsByTitle = async (req, res) => {
+    try {
+        const { title } = req.query;
+
+
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+};
