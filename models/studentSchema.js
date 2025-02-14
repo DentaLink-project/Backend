8e0fe46f4769d75ecf4c74dfddda440f69ef4ddb
sohir <p@gmail.com>
2025-02-14 19:20:28 +0200

    feat: add favorites field to store user's favorite patients in student schema

--- models/studentSchema.js
+++ models/studentSchema.js
@@ -7,6 +7,7 @@ const studentSchema = new mongoose.Schema({
     IDPicture: { type: String, required: true },
     cases: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Case' }],
     tools: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tool' }],
+    favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Patient" }],
     otpCode: { type: String },
     otpExpiresAt: { type: Date }
 }, { timestamps: true })
