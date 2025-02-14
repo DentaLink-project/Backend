fd427da8359a9f6815086f103204b2f9c3543501
sohir <p@gmail.com>
2025-02-14 19:26:18 +0200

    feat: add patient case service for creating new patients

---
+++ services/patientCase/addPatientServices.js
@@ -0,0 +1,29 @@
+import Patient from "../../models/patientCaseSchema.js";
+import { uploadImage } from "../imageService.js";
+
+export const addPatient = async ({ name, title, age, gender, phone, category, description, file, createdBy }) => {
+    try {
+        let imageUrl = null;
+
+        if (!file) {
+            throw new Error("Case file is required");
+        }
+        imageUrl = await uploadImage(file);
+
+        const patient = await Patient.create({
+            name,
+            age: parseInt(age),
+            gender,
+            phone,
+            title,
+            category,
+            description,
+            file: imageUrl,
+            createdBy
+        });
+
+        return patient;
+    } catch (error) {
+        throw new Error(`Error adding patient: ${error.message}`);
+    }
+};
