24925a94c7340fa9487e966dc73a11886c48ce8c
saif-elhady <saifelhady2004@gmail.com>
2025-02-16 18:39:07 +0200

    feat: Update image service to handle multiple file uploads

--- services/imageService.js
+++ services/imageService.js
@@ -4,23 +4,27 @@ import multer from "multer";
 const storage = multer.memoryStorage();
 export const upload = multer({ storage });
 
-export const uploadImage = async (file) => {
+export const uploadImage = async (files) => {
     try {
-        const fileName = `student-${Date.now()}-${file.originalname}`;
+        const uploadedFiles = [];
 
-        const { data, error } = await supabase.storage
-            .from('DentaLink')
-            .upload(fileName, file.buffer, {
-                contentType: file.mimetype
-            });
-        
-        if (error) throw new Error(error.message);
+        for (const file of files) {
+            const fileName = `student-${Date.now()}-${file.originalname}`;
 
-        const { data: urlData } = supabase.storage
-            .from('DentaLink')
-            .getPublicUrl(data.path);
+            const { data, error } = await supabase.storage
+                .from('DentaLink')
+                .upload(fileName, file.buffer, {
+                    contentType: file.mimetype
+                });
+            if (error) throw new Error(error.message);
+
+            const { data: urlData } = supabase.storage
+                .from('DentaLink')
+                .getPublicUrl(data.path)
   
-      return urlData.publicUrl;
+            uploadedFiles.push(urlData.publicUrl);
+        }
+        return uploadedFiles;
     }
     catch (error) {
         throw new Error(`Image upload failed: ${error.message}`);
