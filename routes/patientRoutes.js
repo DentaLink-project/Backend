bc32eac898153fa3f0a4b4fb9d70e910a604f530
sohir <p@gmail.com>
2025-02-16 22:33:29 +0200

    refactor(addPatient): use array to handle multiple images

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -3,24 +3,24 @@ import express from "express";
 //     fetchAllPatients,
 //     fetchPatientById,
 //     fetchPatientsByTitle,
-//     fetchPatientsByUser,
-//     updatePatientStatus} from "../controllers/patientController.js";
+//     fetchPatientsByUser} from "../controllers/patientController.js";
+
 import * as PC from '../controllers/patientController.js'
     
 import { upload } from "../services/imageService.js";
-import { patientCaseValidator, validatePatientUpdate } from "../utils/validation/patientValidator.js";
+import { patientCaseValidator } from "../utils/validation/patientValidator.js";
 import { checkAuth } from "../middleware/checkAuth.js";
 
 const router = express.Router();
 
-router.post("/add", checkAuth, upload.single("file"), patientCaseValidator,PC.createPatient);
+router.post("/add", checkAuth, upload.array("file"), patientCaseValidator,PC.createPatient);
 router.get("/", checkAuth, PC.fetchAllPatients);
 router.post("/toggle", checkAuth, PC.toggleFavoriteController);
 router.get("/favorites", checkAuth, PC.fetchFavoritePatients);
 router.get("/my-patients", checkAuth, PC.fetchPatientsByUser); 
-router.get("/search",PC.fetchPatientsByTitle);
+router.get("/search",checkAuth,PC.fetchPatientsByTitle);
 router.get("/:id" ,checkAuth,PC.fetchPatientById)
-router.put("/:id", checkAuth,validatePatientUpdate, PC.editPatient);
+router.put("/:id", checkAuth, upload.array("file"), patientCaseValidator, PC.editPatient);
 router.delete("/:patientId", checkAuth, PC.deletePatient);
 
 
