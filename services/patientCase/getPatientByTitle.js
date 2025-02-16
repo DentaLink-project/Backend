9133f0ac2389fb6cd3abcce13f5ae0859650af29
sohir <p@gmail.com>
2025-02-16 22:29:09 +0200

    feat(getPatientByTitle): apply full-text search for patient title

--- services/patientCase/getPatientByTitle.js
+++ services/patientCase/getPatientByTitle.js
@@ -2,7 +2,7 @@ import Patient from "../../models/patientCaseSchema.js";
 
 export const getPatientByTitle = async (title) => {
     try {
-        const patients = await Patient.find({ title: { $regex: title, $options: "i" } });
+        const patients = await Patient.find({ $text: { $search: title } });
 
         if (!patients.length) {
             throw new Error("No patients found with this title");
