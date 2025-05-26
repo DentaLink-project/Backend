94e00759601474f7135b5be58264e8e32e7e5617
saif-elhady <saifelhady2004@gmail.com>
2025-05-27 00:03:24 +0300

    edit

--- services/cart/removeitemService.js
+++ services/cart/removeitemService.js
@@ -5,7 +5,16 @@ export const removeFromCart = async (studentId, toolId) => {
         const cart = await Cart.findOne({ student: studentId });
         if (!cart) throw new Error("Cart not found");
 
-        cart.items = cart.items.filter(item => item.tool.toString() !== toolId);
+        const item = cart.items.find(item => item.tool.toString() === toolId);
+        if (!item) throw new Error("Item not found in cart");
+
+        if (item.quantity > 1) {
+            item.quantity -= 1;
+        } else {
+            cart.items = cart.items.filter(item => item.tool.toString() !== toolId);
+        }
+
+        await cart.populate("items.tool");
 
         cart.totalPrice = cart.items.reduce((total, item) => {
             return total + item.quantity * item.tool.price;
