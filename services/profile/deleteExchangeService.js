2fd1f427be7e42aefb569aa5ebdcb32ad5756efe
saif-elhady <saifelhady2004@gmail.com>
2025-05-26 17:58:25 +0300

    edit

--- services/profile/deleteExchangeService.js
+++ services/profile/deleteExchangeService.js
@@ -1,7 +1,7 @@
 import Exchange from "../../models/exchangeSchema.js";
 
 export const deleteExchangeService = async (exchangeId, studentId) => {
-    const exchange = await Exchange.findOne({ _id: exchangeId, publisher: studentId });
+    const exchange = await Exchange.findOne({ _id: exchangeId, createdBy: studentId });
 
     if (!exchange) {
         throw new Error("Exchange not found or you do not have permission to delete it");
